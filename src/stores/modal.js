0edf93d8c4f7ee8a57d7f5c812882fb050581356
unknown <ionut_chirita@yahoo.com>
2024-03-12 21:48:19 +0200

    using getters

--- src/stores/modal.js
+++ src/stores/modal.js
@@ -1,7 +1,12 @@
 import { defineStore } from 'pinia'
 
 export default defineStore('modal', {
-    state: () => ({
-        isOpen: false
-    })
+  state: () => ({
+    isOpen: false
+  }),
+  getters: {
+    hiddenClass(state) {
+      return !state.isOpen ? 'hidden' : ''
+    }
+  }
 })
