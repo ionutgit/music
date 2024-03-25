fd598ec6d9936c3375a91a4635de99b6c3bc1bbd
unknown <ionut_chirita@yahoo.com>
2024-03-25 22:01:24 +0200

    Path Parameters:

--- src/router/index.js
+++ src/router/index.js
@@ -2,22 +2,23 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import About from '@/views/About.vue'
 import Manage from '@/views/Manage.vue'
+import Song from '@/views/Song.vue'
 import useUserStore from '@/stores/user'
 
 const routes = [
   {
     name: 'home',
-    path: '/', 
+    path: '/',
     component: Home
-  }, 
+  },
   {
     name: 'about',
-    path: '/about', 
+    path: '/about',
     component: About
-  }, 
+  },
   {
     name: 'manage',
-    path: '/manage-music', 
+    path: '/manage-music',
     component: Manage,
     // beforeEnter: (to, from, next) => {
     //   console.log('beforeEnter Manage Guard');
@@ -28,36 +29,39 @@ const routes = [
     }
   },
   {
-    path: '/manage', 
+    path: '/manage',
     redirect: { name: 'manage' }
+  },
+  {
+    name: 'song',
+    path: '/song/:id',
+    component: Song
   },
   {
     path: '/:catchAll(.*)*',
     redirect: { name: 'home' }
   }
-];
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-  linkExactActiveClass: "text-yellow-500"
+  linkExactActiveClass: 'text-yellow-500'
 })
 
 router.beforeEach((to, from, next) => {
-  
-  if(!to.meta.requiresAuth) {
-    next();
-    return;
+  if (!to.meta.requiresAuth) {
+    next()
+    return
   }
 
-  const store = useUserStore();
+  const store = useUserStore()
 
   if (store.userLoggedIn) {
-    next();
+    next()
   } else {
     next({ name: 'home' })
   }
-
 })
 
 export default router
