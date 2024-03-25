5a2cd5fcea888b1727ef67ffc800361310b89892
unknown <ionut_chirita@yahoo.com>
2024-03-25 22:37:52 +0200

    Validating the Comment

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -29,15 +29,18 @@
         <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
       </div>
       <div class="p-6">
-        <form>
-          <textarea
+        <vee-form :validation-schema="schema">
+          <vee-field
+            as="textarea"
+            name="comment"
             class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded mb-4"
             placeholder="Your comment here..."
-          ></textarea>
+          />
+          <ErrorMessage class="text-red-600" name="comment" />
           <button type="submit" class="py-1.5 px-3 rounded text-white bg-green-600 block">
             Submit
           </button>
-        </form>
+        </vee-form>
         <!-- Sort Comments -->
         <select
           class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
@@ -131,7 +134,10 @@ export default {
   name: 'Song',
   data() {
     return {
-      song: {}
+      song: {},
+      schema: {
+        comment: 'required|min:3'
+      }
     }
   },
   async created() {
