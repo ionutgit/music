f7505f4201ce61a829964ec2b0c3854f3c875e17
ionutgit <ionut_chirita@yahoo.com>
2024-03-28 12:17:15 +0200

    Updating the Comment Count

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -25,7 +25,7 @@
     <div class="bg-white rounded border border-gray-200 relative flex flex-col">
       <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
         <!-- Comment Count -->
-        <span class="card-title">Comments (15)</span>
+        <span class="card-title">Comments ({{ song.comment_count }})</span>
         <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
       </div>
       <div class="p-6">
@@ -108,7 +108,7 @@ export default {
       if (newVal === this.$route.query.sort) {
         return;
       }
-      
+
       this.$router.push({
         query: {
           sort: newVal
@@ -144,6 +144,11 @@ export default {
       }
 
       await commentsCollection.add(comment)
+
+      this.song.comment_count += 1;
+      await songsCollenction.doc(this.$route.params.id).update({
+        comment_count: this.song.comment_count
+      })
 
       this.getComments();
 
