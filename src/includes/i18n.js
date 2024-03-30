fb926f1cd57139f4e59d925706b770582ceb0363
ionutgit <ionut_chirita@yahoo.com>
2024-03-30 09:41:53 +0200

    our first translation

--- src/includes/i18n.js
+++ src/includes/i18n.js
@@ -1,10 +1,12 @@
 import { createI18n } from 'vue-i18n'
 import en from '@/locales/en'
+import fr from '@/locales/fr'
 
 export default createI18n({
   locale: 'en',
   fallbackLocale: 'en',
   messages: {
-    en
+    en, 
+    fr
   }
 })
