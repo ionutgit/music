18676ce002a8e21a727bcf3afdff786734df8916
unknown <ionut_chirita@yahoo.com>
2024-03-30 08:02:46 +0200

    i18n: configuration step

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import { createPinia } from 'pinia'
 import VeeValidate from '@/includes/validation'
 import { auth } from './includes/firebase'
 import Icon from './directives/icon'
+import I18n from './includes/i18n'
 
 import App from './App.vue'
 import router from './router'
@@ -18,6 +19,7 @@ auth.onAuthStateChanged(() => {
     app.use(createPinia())
     app.use(router)
     app.use(VeeValidate)
+    app.use(I18n)
     app.directive('icon', Icon)
 
     app.mount('#app')
