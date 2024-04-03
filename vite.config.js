b0d8c68172204ffbe3477ec990230c1bdfa0bb35
ionutgit <ionut_chirita@yahoo.com>
2024-04-03 19:47:56 +0300

    optimization: rollupvisualizer

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 import vue from '@vitejs/plugin-vue'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -28,7 +29,8 @@ export default defineConfig({
       workbox: {
         globPatterns: ["**/*.{js,css,html,png,jpg,}"]
       }
-    })
+    }), 
+    visualizer({ open: true })
   ],
   resolve: {
     alias: {
