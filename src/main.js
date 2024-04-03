988987eeb0b9cec287a4ea85613480b0157e94b6
ionutgit <ionut_chirita@yahoo.com>
2024-04-03 19:34:21 +0300

    optimization: progress bar

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './assets/base.css'
 import './assets/main.css'
+import 'nprogress/nprogress.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -9,11 +10,14 @@ import Icon from './directives/icon'
 import I18n from './includes/i18n'
 import { registerSW } from "virtual:pwa-register"
 import GlobalComponents from "./includes/_globals"
+import progressBar from './includes/progress-bar'
 
 import App from './App.vue'
 import router from './router'
 
 registerSW({ immediate: true })
+
+progressBar(router);
 
 let app
 auth.onAuthStateChanged(() => {
