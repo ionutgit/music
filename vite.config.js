0d0aaba3f679b49bbbcb1604d230ae82e452f5b1
ionutgit <ionut_chirita@yahoo.com>
2024-04-04 10:25:57 +0300

    remove rollup vizualizer

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 import vue from '@vitejs/plugin-vue'
-import { visualizer } from 'rollup-plugin-visualizer'
+// import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -30,7 +30,7 @@ export default defineConfig({
         globPatterns: ["**/*.{js,css,html,png,jpg,}"]
       }
     }), 
-    visualizer({ open: true })
+    // visualizer({ open: true })
   ],
   resolve: {
     alias: {
